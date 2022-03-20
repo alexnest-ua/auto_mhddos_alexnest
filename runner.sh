397d39fcb3abe144742c3fcfa7f22ccf3799b460
alexnest-ua <74729549+alexnest-ua@users.noreply.github.com>
2022-03-20 12:13:56 +0200

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -42,7 +42,7 @@ git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
 cd mhddos_proxy
 sudo rm proxies_config.json
 sudo wget https://raw.githubusercontent.com/Aruiem234/mhddosproxy/main/proxies_config.json
-git clone https://github.com/alexnest-ua/MHDDoS.git
+git clone https://github.com/MHProDev/MHDDoS.git
 sudo python3 -m pip install -r MHDDoS/requirements.txt
 cd ~
 
