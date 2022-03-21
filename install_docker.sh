b5b5bd49a3b29524e9280bd43955844d9e8f3b44
alexnest-ua <74729549+alexnest-ua@users.noreply.github.com>
2022-03-21 18:23:26 +0200

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo echo 'deb [arch=amd64] https://download.docker.com/linux/debian buster stable' | sudo tee /etc/apt/sources.list.d/docker.list
 sudo apt update -y
