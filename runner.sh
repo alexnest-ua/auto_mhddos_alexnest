78718fb9edb61c1b413a8356509b2e34277e852c
alexnest-ua <74729549+alexnest-ua@users.noreply.github.com>
2022-05-11 02:10:26 +0300

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -6,7 +6,7 @@ sudo pkill -e -f runner.py
 sudo pkill -e -f finder.py
 echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;35mAll old processes with DDoS and finder\033[0;0m\n"
 
-echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33mInstalling our proxy_finder...\033[0;0m\n"
+echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33mInstalling our new proxy_finder...\033[0;0m\n"
 sleep 3s
 cd ~
 sudo rm -rf proxy_finder
