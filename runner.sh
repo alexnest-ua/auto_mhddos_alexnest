8edc29c5726a2afdf3d1c4e55e5cedce3f570916
alexnest-ua <74729549+alexnest-ua@users.noreply.github.com>
2022-05-11 23:29:09 +0300

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -83,7 +83,7 @@ while [ 1 == 1 ]
 do	
 	cd ~/mhddos_proxy
 	
-	num0=$(sudo git pull origin main | grep -P -c 'Already|Уже')
+	num0=$(sudo git pull origin main | grep -E -c 'Already|Уже')
    	echo "$num0"
    	
    	if ((num0 == 1));
@@ -99,7 +99,7 @@ do
 	
 	cd ~/proxy_finder	
 
-	num0=$(sudo git pull origin main | grep -P -c 'Already|Уже')
+	num0=$(sudo git pull origin main | grep -E -c 'Already|Уже')
    	echo "$num0"
    	
    	if ((num0 == 1));
@@ -114,7 +114,7 @@ do
 	fi
 	
 	cd ~/auto_mhddos_alexnest
-   	num=$(sudo git pull origin main | grep -P -c 'Already|Уже')
+   	num=$(sudo git pull origin main | grep -E -c 'Already|Уже')
    	echo "$num"
    	
    	if ((num == 1));
