0729ed8e30bfa70fdfcc4f48253e2ebe1766637e
alexnest-ua <74729549+alexnest-ua@users.noreply.github.com>
2022-05-18 15:01:46 +0300

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -164,6 +164,16 @@ then
 			num_of_copies=1
 		fi
 	fi
+
+elif ((proc_num > 4));
+then
+	if ((threads < 4000));
+	then
+		echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[0;33m$threads is too LOW amount of threads for $proc_num CPUs - attack will be started with 4000 async threads\033[0;0m\n"
+		threads=4000
+	fi
+
+fi
 	
 fi
 
