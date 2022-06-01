883bd8a35338e0ef92a11b48aad77229ad319448
alexnest-ua <74729549+alexnest-ua@users.noreply.github.com>
2022-06-02 01:16:04 +0300

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -22,7 +22,7 @@ then
 fi
 
 
-num=$(python3 --version | grep -E -c '3.10')
+num=$(python3 --version | grep -E -c '3.9|3.10')
 if ((num == 1));
 then	
 	echo -e "[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - Python3 is already up to date"
