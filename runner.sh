b8edf5eec72b82433e473f83c5fc57ba60a262a9
alexnest-ua <74729549+alexnest-ua@users.noreply.github.com>
2022-06-06 04:55:28 +0300

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -257,8 +257,10 @@ do
 		if ((num_of_copies > list_size));
 		then 
 			random_numbers=$(shuf -i 1-$list_size -n $list_size)
+			copies=$num_of_copies
 		else
 			random_numbers=$(shuf -i 1-$list_size -n $num_of_copies)
+			copies=1
 		fi
 	
 		echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - Random number(s): " $random_numbers "\n"
@@ -277,7 +279,7 @@ do
             		echo "python3 runner.py $cmd_line --rpc $rpc -t $threads --vpn $debug"
             
             		cd ~/mhddos_proxy
-            		AUTO_MH=1 python3 runner.py $cmd_line -t $threads --vpn $debug&
+            		AUTO_MH=1 python3 runner.py $cmd_line -t $threads --copies $copies --vpn $debug&
 	    		sleep 20s
             		echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[42mAttack started successfully\033[0m\n"
    		done
@@ -294,8 +296,10 @@ do
 		if ((num_of_copies > list_size));
 		then 
 			random_numbers=$(shuf -i 1-$list_size -n $list_size)
+			copies=$num_of_copies
 		else
 			random_numbers=$(shuf -i 1-$list_size -n $num_of_copies)
+			copies=1
 		fi
 	
 		echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - Random number(s): " $random_numbers "\n"
@@ -313,7 +317,7 @@ do
             		echo "python3 runner.py $cmd_line --rpc $rpc -t $threads --vpn $debug"
             
             		cd ~/mhddos_proxy
-            		AUTO_MH=1 python3 runner.py $cmd_line -t $threads --vpn $debug&
+            		AUTO_MH=1 python3 runner.py $cmd_line -t $threads --copies $copies --vpn $debug&
 	    		sleep 20s
 			echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[42mAttack started successfully\033[0m\n"
 			
